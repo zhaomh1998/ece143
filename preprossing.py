ece698fa84f906c5c887a6b7380ce01f0d43c97a
Scott Zhao <zhaomh1998@outlook.com>
2018-11-13 23:15:42 -0800

    Init data cleaning

--- preprossing.py
+++ preprossing.py
@@ -4,7 +4,8 @@ import pandas as pd
 def load_csv(data_path):
     """
     Loads the data set and return a pandas DataFrame
-    :return: pandas DataFrame
+    :param str data_path: Path to data csv file
+    :return: pandas DataFrame read from the file
     """
     from os import path
     assert isinstance(data_path, str)
@@ -13,6 +14,27 @@ def load_csv(data_path):
     return pd.read_csv(data_path)
 
 
+def clean_data(original_data):
+    """
+    Filter out unwanted and invalid data from the data set
+    :param DataFrame original_data: Original DataFrame read from data csv
+    :return: Filtered pandas DataFrame
+    """
+    assert isinstance(original_data, pd.DataFrame)
+    filtered_data = original_data.copy()
+    filtered_data.drop(['v18q1'], axis=1, inplace=True)
+    # TODO: Add other filtering
+    return filtered_data
+
+
+def get_training_set():
+    """
+    Load training.csv and clean it
+    :return: Cleaned train.csv pandas DataFrame
+    """
+    return clean_data(load_csv('train.csv'))
+
+
 if __name__ == '__main__':
-    data = load_csv('train.csv')
+    data = get_training_set()
     print(data)
